e0cc09e77dca697336f68e5a204293eced731b88
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-02-09 09:44:47 +0530

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -53,6 +53,8 @@ let jsonSample = [
 ]
 
 
+
+
 let result = false;
 for(let js of jsonSample){
     if((js.userName === "vignesh" && js.password === "vignesh") || (js.userName === "Kumar" && js.password === "kumar")){
@@ -76,6 +78,7 @@ if(result){
 //     "email": "abc@gmail.com",
 //     "mobile": 3412242
 // }
+
 
 let nestedJson = [
     {
@@ -118,3 +121,79 @@ for(let i of nestedJson){
     }
 
 }
+
+let i = 0;
+while(i <4){
+    // console.log(i);
+    i++;  
+}
+
+let sampleArray = [1,2,3,4,5]
+
+let j = 0;
+while(j < sampleArray.length){
+    // console.log(sampleArray[j])
+    j++;
+}
+
+sampleArray.forEach((e) => 
+console.log(e)
+);
+
+let evenOdd = [10,40,33,9, 12]
+let num = 0;
+
+while(num < evenOdd.length){
+    if(evenOdd[num] %2 != 0){
+        console.log("odd number", evenOdd[num])
+    }
+    num++;
+}
+
+let nameN = "fedfsfds"
+console.log(nameN.length)
+console.log(nameN.toUpperCase())
+console.log(nameN.toLowerCase())
+console.log(nameN.charAt(0))
+console.log(nameN[0])
+console.log(nameN.startsWith("fed"))
+console.log(nameN.endsWith("ds"))
+console.log(nameN.includes("fs"))
+console.log(nameN.slice(0, 3))
+console.log(nameN.substring(4))
+
+let s = " hi team "
+console.log(s.replaceAll("hi", "hello"))
+let n = s.replaceAll(" ", "")
+let ef = n.split("")
+console.log(ef)
+console.log(s)
+console.log(s.trim())
+console.log(s.trimEnd())
+
+
+// function concept
+
+
+let resultNew = false;
+
+function loginValidation() {
+    for(let js of jsonSample){
+    if((js.userName === "vignesh" && js.password === "vignesh") || (js.userName === "Kumar" && js.password === "kumar")){
+      console.log("The username password match successful");
+        return true;
+    }
+}
+return false;
+}
+
+function multiplyFunction(a1,b1){
+    return a1*b1;
+}
+
+// resultNew = loginValidation();
+console.log(resultNew);
+
+console.log(multiplyFunction(10,20))
+
+
