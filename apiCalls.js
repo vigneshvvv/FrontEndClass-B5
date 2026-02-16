a21922b7772dd3e21e1512f6b3352d1fe2fbb956
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-02-16 09:38:08 +0530

    Update apiCalls.js

--- apiCalls.js
+++ apiCalls.js
@@ -7,3 +7,36 @@
     .then((e) => e.json()).then((e1) => console.log(e1.products))
 
 }
+
+
+async function postTitleData(){
+    await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json" 
+        },
+        body: JSON.stringify({
+            userID: 11,
+            id: 101,
+            title: "sample testing",
+            body: "sdfsasdsddsdasdsdsd"
+        })
+    }).then((response) => console.log(response))
+}
+
+
+ let id = 1;
+async function putTitleData(){
+    await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type" : "application/json" 
+        },
+        body: JSON.stringify({
+            userID: 11,
+            id: 1,
+            title: "sample testing",
+            body: "sdfsasdsddsdasdsdsd"
+        })
+    }).then((response) => console.log(response))
+}
